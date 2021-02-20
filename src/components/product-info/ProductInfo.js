c8601495cc9e2021199a1f533800fc7febece03c
Thiago Rodrigues <=>
2021-02-20 17:23:32 -0300

    ajustes tela de informações do produto

--- src/components/product-info/ProductInfo.js
+++ src/components/product-info/ProductInfo.js
@@ -1,17 +1,27 @@
 import React from 'react'
 import './product-info.scss'
 
-export default function ProductInfo({isFetching, product}) {
+export default function ProductInfo({ product, addProduct, removeProduct, showProductDetails }) {
+    const goBackToProductList = () => {
+        showProductDetails()
+    }
 
-    const handleBuyProduct = (e) => {
-        e.preventDefault()
+    const handleAddProduct = (e, id) => {
+        addProduct(e, id)
+        // dispatch('ADD_PRODUCT', { a: 'a'})
+        // Ao clicar em comprar, e o produto não estiver no carrinho, deve ser adicionado
+        // Ao clicar em comprar, e o produto ja existir no carrinho, deve ser incrementado em 1
+    }
+
+    const handleRemoveProduct = (e) => {
+        removeProduct(e)
+        // dispatch('ADD_PRODUCT', { a: 'a'})
         // Ao clicar em comprar, e o produto não estiver no carrinho, deve ser adicionado
         // Ao clicar em comprar, e o produto ja existir no carrinho, deve ser incrementado em 1
     }
 
     return (
         <div className="product-info container">
-            <button className="product-info__back-button" onClick={() => console.log('voltar')}>Voltar</button>
             <h1 className="product-info__title">{product.title}</h1>
             <img className="product-info__picture" alt="produto" src={product.picture} />
             <p className="product-info__text">{product.description}</p>
@@ -19,8 +29,13 @@ export default function ProductInfo({isFetching, product}) {
             <p className="product-info__text"><span>Tipo do Chip: </span>{product.chipType}</p>
             <p className="product-info__text"><span>Memória: </span>{product.memory}</p>
             <p className="product-info__text"><span>Em estoque: </span>{product.quantity}</p>
-            <p className="product-info__price">R$ {product.price}</p>
-            <button className="product-info__buy" onClick={e=> handleBuyProduct(e)}>Comprar</button>
+            <p className="product-info__price">{product.quantity === 0 && 'Último preço'} R$ {product.price}</p>
+            <button className="product-info__back-button" onClick={() => goBackToProductList()}>Voltar</button>
+            {/* <div className="product-info__group-buttons">
+                {product.quantity === 0 ? <p>Produto indisponível</p>: (
+                    <button className="product-info__group-buttons--buy" onClick={e => handleAddProduct(e, product.id)}>Comprar</button>
+                    )}
+            </div> */}
         </div>
     )
 }
--- src/components/product-list/ProductsList.js
+++ src/components/product-list/ProductsList.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import ProductUtils from '../../utils/ProductUtils'
 
-export default function ProductList({isFetching, products}) {
+export default function ProductList({isFetching, products, showProductDetails}) {
     return (
         <React.Fragment>
             {!isFetching && products.map((product, idx) => (
-              <div key={`product-${idx}`} className="home__card-product">
+              <div key={`product-${idx}`} className="home__card-product" onClick={() => showProductDetails(idx)}>
                 <h2 className="home__card-product--title">{ProductUtils.formatProductTitle(product.title)}</h2>
                 <img alt="produto" src={product.picture} />
                 <p className="home__card-product--price">R$ {product.price}</p>
