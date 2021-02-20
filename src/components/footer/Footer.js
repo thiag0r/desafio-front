2a6476f0eb6348c13415c6da0d527496f54237fa
Thiago Rodrigues <=>
2021-02-20 15:05:02 -0300

    adicionando informações no footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,8 +3,13 @@ import './footer.scss'
 
 export default function Footer () {
     return (
-        <footer>
-            
+        <footer className="Footer">
+            <div className="Footer__top-info">
+                <p><strong>thiagorodrigues15@hotmail.com.br</strong></p>
+            </div>
+            <div className="Footer__bottom-info">
+                <p>Desenvolvido por <span className="Footer__bottom-info--my-name"> Thiago Rodrigues</span></p>
+            </div>
         </footer>
     )
 }
